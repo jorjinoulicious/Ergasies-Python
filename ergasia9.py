1d3259d1671e9fe4293a6e003c7c9daa1d6f78d7
jorjinoulicious <tzinaaa15@gmail.com>
2022-10-06 01:51:40 +0300

    Added comments

--- ergasia9.py
+++ ergasia9.py
@@ -1,11 +1,13 @@
 def pair(lst,t):
 
-    for i in range(len(lst)):
-        if (lst[i] == ""):
+    for i in range(len(lst)):                   # For every item in the list
+        if (lst[i] == ""):                      # If it is the empty string remove it
             continue
-        for j in range(i+1,len(lst)):
-            if (lst[j] != "" and lst[i]!= ""):
-                if ((lst[i] + lst[j]) == t):
+        for j in range(i+1,len(lst)):           # For every item next to the i-item
+            if (lst[j] != "" and lst[i]!= ""):  # If each list item is an int and not a string, do the following:
+                if ((lst[i] + lst[j]) == t):    # If the 2 nearby list items(pair) sum to t
+
+                    # Replace them with an empty string so we know which pairs to remove later on
                     lst[i] = ""
                     lst[j] = ""
 
